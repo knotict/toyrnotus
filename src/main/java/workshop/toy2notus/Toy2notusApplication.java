d48286861978f205af942bb67062500a58d27614
IRONMAN <ironman@IRONMANs-Mac-mini.local>
2018-04-24 11:29:29 +0700

    First commit ja

---
+++ src/main/java/workshop/toy2notus/Toy2notusApplication.java
@@ -0,0 +1,12 @@
+package workshop.toy2notus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toy2notusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Toy2notusApplication.class, args);
+	}
+}
---
+++ src/test/java/workshop/toy2notus/Toy2notusApplicationTests.java
@@ -0,0 +1,18 @@
+package workshop.toy2notus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Toy2notusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+
+
+}
